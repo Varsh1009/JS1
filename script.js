e0a8b4b74c6a5279df1816091eda99b9ce686bfa
Varsh1009 <83211221+Varsh1009@users.noreply.github.com>
2022-03-11 10:28:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+//DIFFERENT DATA TYPES
+
+
+var myName="Shrivarshini"; // String 
+console.log(myName);
+var a; // integer
+a = 7;
+console.log(a);
+let length = 3.54; //float
+console.log(length);
+var x = false;  //boolean
+console.log(x);
+
+
+
+// AREA OF CIRCLE
+ 
+
+const pi=3.14;
+let radius= 3;
+var area= pi*radius*radius;
+console.log(area);
+
+var perimeter= 2*pi*radius;
+console.log(perimeter);
+
+console.log(radius);
